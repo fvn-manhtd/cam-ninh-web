5714ee87ab9b4a0f204e5fa9d9c97c115a5d66d1
fvn-manhtd <fvn.manhtd@gmail.com>
2022-12-12 07:10:16 +0000

    Deploying to gh-pages from @ fvn-manhtd/cam-ninh-web@b34fd2238f0cd0e1014c09870da15ba24c5c9746 🚀

--- _next/static/91d1HfOWGiQsgqOMSZmYO/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/121-3bbe128eca740767.js","static/css/349b8f6fbf6f9a3a.css","static/chunks/pages/index-4b83adf6de2f588e.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
